1f355a1ff8665345523482bd9b71cd66d9380bc9
Babi-B <babibyoolah@gmail.com>
2023-10-19 06:17:02 +0100

    refactoring code. syntax cleanup

--- src/components/MiniNavbar.jsx
+++ src/components/MiniNavbar.jsx
@@ -8,8 +8,8 @@ export default function MiniNavbar(props) {
                 <div className="cust-grad">
                     <div className="container-lg">
                         <div className="d-flex flex-wrap justify-content-between text-white py-5">
-                            <h4>{props.location}</h4>
-                            <h4><Link to="/" className="text-white">Home</Link> | <Link to={props.location_url} className="text-info">{props.location}</Link></h4>
+                            <h4 className="mb-3">{props.location}</h4>
+                            <h4><Link to="/" className="text-white mx-3">Home</Link> | <Link to={props.location_url} className="text-info mx-3">{props.location}</Link></h4>
                         </div>        
                     </div>
                 </div>
